7f9812f9b9fd2d28094f3e5675dc81596dfdb731
Lean-b <pentestinglean@gmail.com>
2023-12-08 17:54:12 +0000

    Test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 use std::{fs::File,path::Path,io::{self, BufRead}};
 
 
-
-
-
-
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
     let file = File::open(filename)?;
@@ -20,7 +16,6 @@ fn from_hex_to_text(buffer: &[u8]) -> String{
 
 
 fn main(){
-
     if let Ok(lines) =  read_lines("hexx.txt"){
         for line in lines{
             if let Ok(line_content) = line{
@@ -29,6 +24,44 @@ fn main(){
                 println!("{}", convert_to_text);
             } else {
                 print!("Error");
+            }
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod test{
+    use crate::from_hex_to_text;
+
+    use super::read_lines;
+    
+    #[test]
+    fn test_read_file(){
+        if let Ok(lines) = read_lines("hexx.txt"){
+            for line in lines{
+                println!("{:?}", line);
+                assert_eq!(
+                    line.unwrap(),
+                    "48 6f 6c 6c 6f 20 77 6f 72 6c 64"
+                );
+            }
+        } else {
+            print!("Error")
+        }
+    }
+
+    #[test]
+    fn test_from_hex_to_text(){
+        if let Ok(lines) =  read_lines("hexx.txt"){
+            for line in lines{
+                if let Ok(line_content) = line{
+                    let clean = line_content.replace(" ", "");
+                    let convert_to_text = from_hex_to_text(clean.as_bytes());
+                    println!("{}", convert_to_text);
+                } else {
+                    print!("Error");
+                }
             }
         }
     }
